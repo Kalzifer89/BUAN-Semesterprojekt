0c0ae55b1b9e8b2c4b0e51393d7c650d2ac9d636
Kalzifer89 <sven.krumbeck@gmail.com>
2019-10-17 14:15:40 +0200

    Datenbank Dump

--- datanbankdump/krumbeck.sql
+++ datanbankdump/krumbeck.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 16. Okt 2019 um 13:36
+-- Erstellungszeit: 17. Okt 2019 um 14:15
 -- Server-Version: 10.1.38-MariaDB
 -- PHP-Version: 7.3.3
 
@@ -21,6 +21,76 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `krumbeck`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `categorys`
+--
+
+CREATE TABLE `categorys` (
+  `categoryID` int(10) NOT NULL,
+  `categoryNameDE` varchar(50) COLLATE utf8_bin NOT NULL,
+  `categoryNameENG` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Daten für Tabelle `categorys`
+--
+
+INSERT INTO `categorys` (`categoryID`, `categoryNameDE`, `categoryNameENG`) VALUES
+(1, 'Handys', 'Mobiles'),
+(2, 'Laptops', 'Laptops');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `items`
+--
+
+CREATE TABLE `items` (
+  `orderID` int(10) NOT NULL,
+  `productID` int(10) NOT NULL,
+  `productQuantity` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderID` int(10) NOT NULL,
+  `userID` int(10) NOT NULL,
+  `itemID` int(10) NOT NULL,
+  `orderDate` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `products`
+--
+
+CREATE TABLE `products` (
+  `productID` int(10) NOT NULL,
+  `productNameDE` varchar(50) COLLATE utf8_bin NOT NULL,
+  `productNameENG` varchar(50) COLLATE utf8_bin NOT NULL,
+  `productDescriptionDE` text COLLATE utf8_bin NOT NULL,
+  `productDescriptionENG` text COLLATE utf8_bin NOT NULL,
+  `productImage` varchar(50) COLLATE utf8_bin NOT NULL,
+  `productCategoryID` int(10) NOT NULL,
+  `productPrice` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Daten für Tabelle `products`
+--
+
+INSERT INTO `products` (`productID`, `productNameDE`, `productNameENG`, `productDescriptionDE`, `productDescriptionENG`, `productImage`, `productCategoryID`, `productPrice`) VALUES
+(1, 'Iphone 8 (Grau)', 'Iphone 8  (Grey)', 'Das iPhone 8 Plus ist eine neue iPhone Generation. Es ist aus stabilem Glas gebaut, mit robusterem Rahmen aus einem Aluminium, das auch in der Raumfahrt verwendet wird.\r\n\r\nEs lädt kabellos.\r\nUnd es ist vor Spritzwasser und Staub geschützt.\r\nHat ein 5,5\" Retina HD Display mit True Tone.\r\nEine 12 Megapixel Dual-Kamera mit verbessertem Porträtmodus und neuem Porträtlicht.\r\nUnd der A11 Bionic ist der leistungsstärkste und intelligenteste Smartphone Chip aller Zeiten.\r\nEr unterstützt Augmented Reality Erlebnisse in Spielen und Apps.\r\nIntelligenz hat noch nie so gut ausgesehen wie beim iPhone 8.', 'The iPhone 8 and 8 Plus feature glass bodies that enable wireless charging, faster A11 processors, upgraded cameras, and True Tone displays. Launched on September 22, 2017.\r\n\r\n4.7 and 5.5-inch LCD display\r\nFaster A11 processor\r\nGlass body\r\nUpgraded camera\r\nLouder speakers\r\nWireless inductive charging', 'image2.jpg', 1, 200),
+(2, 'Macbook 2014', 'Macbook 2014', 'Dünn. Leicht. Leistungsstark. Und bereit für alles.\r\nWas immer die Aufgabe ist – die Intel Core i5 und i7 Prozessoren der 5. Generation mit Intel HD Graphics 6000 sind ihr gewachsen. Alles geht unglaublich schnell, egal ob du Fotos bearbeitest oder im Web surfst. Die gesamte Power steckt in einem nur 1,7 cm dünnen Unibody-Design, das gerade einmal 1,35 kg wiegt.\r\n\r\n802.11ac WLAN. Kabellos. Mühelos.\r\nWenn du dich direkt über eine 802.11ac Basisstation verbindest, erlebst du bis zu 3x schnelleres WLAN als mit der vorherigen Generation. Und 802.11ac ist nicht nur schneller, sondern hat auch mehr Reichweite – und du kannst noch freier arbeiten.\r\n\r\nSSD Speicher. Und es läuft.\r\nDer SSD Speicher im MacBook Air ist bis zu 17x schneller als eine Festplatte mit 5400 U/Min. So ist alles flüssiger und reagiert schneller. Und dank SSD Speicher und Intel Core Prozessoren der fünften Generation wacht das MacBook Air extrem schnell aus dem Ruhezustand auf.\r\n\r\nmacOS\r\nmacOS ist das Betriebssystem hinter allem, was du auf einem Mac machst. macOS High Sierra bringt neue zukunftsweisende Technologien und verbesserte Features auf deinen Mac. Das ist macOS auf hoher Niveau.', '\r\nMacBook Pro features a new quad-core Intel processor for up to 90 percent faster performance.¹ A brilliant and colorful Retina display with True Tone technology for a more comfortable viewing experience. Touch ID. The latest Apple-designed keyboard. And dynamic, contextual controls with Touch Bar. So you can take productivity to the next level.\r\nFeatures\r\nQuad-core 8th-generation Intel Core i5 processor\r\n\r\nBrilliant Retina display with True Tone technology\r\n\r\nTouch Bar and Touch ID\r\n\r\nIntel Iris Plus Graphics 645\r\n\r\nUltrafast SSD\r\n\r\nTwo Thunderbolt 3 (USB Type-C) ports\r\n\r\nUp to 10 hours of battery life²\r\n\r\n802.11ac Wi-Fi\r\n\r\nLatest Apple-designed keyboard\r\n\r\nForce Touch trackpad\r\n\r\nAvailable in space gray and silver\r\n\r\nmacOS Mojave, inspired by pros but designed for everyone, with Dark Mode, Stacks, easier screenshots, useful built-in apps, and more\r\n\r\n¹ Compared to previous 13-inch MacBook Pro with dual-core processor.\r\n\r\n² Battery life varies by use and configuration. See www.apple.com/batteries for more information.', 'macbook2016.jpg', 2, 150);
 
 -- --------------------------------------------------------
 
@@ -59,6 +129,24 @@ INSERT INTO `users` (`userID`, `userName`, `userMail`, `userPassword`, `userSecr
 --
 
 --
+-- Indizes für die Tabelle `categorys`
+--
+ALTER TABLE `categorys`
+  ADD PRIMARY KEY (`categoryID`);
+
+--
+-- Indizes für die Tabelle `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderID`);
+
+--
+-- Indizes für die Tabelle `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productID`);
+
+--
 -- Indizes für die Tabelle `users`
 --
 ALTER TABLE `users`
@@ -67,6 +155,24 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT für exportierte Tabellen
 --
+
+--
+-- AUTO_INCREMENT für Tabelle `categorys`
+--
+ALTER TABLE `categorys`
+  MODIFY `categoryID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT für Tabelle `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `products`
+--
+ALTER TABLE `products`
+  MODIFY `productID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
